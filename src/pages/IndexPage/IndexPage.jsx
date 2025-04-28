8ebd2b7c0b2d48cec95de2753c73b413fa6a8b6a
Milan Sony <milansonyofficial@gmail.com>
2025-04-28 19:53:47 +0530

    style: homepage route added

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 function IndexPage() {
     return (
@@ -11,9 +12,9 @@ function IndexPage() {
                 <div>
                     <h1 className="text-5xl font-bold">StatusCast</h1>
                     <p className="py-6">
-                        “Broadcast Your Presence, Anywhere”
+                        “Broadcast Your Presence, Online”
                     </p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <Link to={"/home"} className="btn btn-primary">Get Started</Link>
                 </div>
             </div>
         </div>
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from "react-router";
 import IndexPage from '../pages/IndexPage/IndexPage';
+import HomePage from '../pages/HomePage/HomePage';
 
 function Router() {
     return (
         <Routes>
             <Route path='/' element={<IndexPage />} />
+            <Route path='/home' element={<HomePage />} />
         </Routes>
     )
 }
