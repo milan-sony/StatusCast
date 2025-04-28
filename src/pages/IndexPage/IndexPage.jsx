9179449d0129e9c64948b0189d2fbda1cccbffec
Milan Sony <milansonyofficial@gmail.com>
2025-04-28 22:02:02 +0530

    style: index page img size reduced

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router'
 
 function IndexPage() {
     return (
-        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero bg-base-200 w-full h-screen">
             <div className="hero-content flex-col lg:flex-row">
                 <img
                     src="/indeximg.png"
-                    className="max-w-sm"
+                    className="w-md"
                 />
                 <div>
-                    <h1 className="text-5xl font-bold">StatusCast</h1>
-                    <p className="py-6">
+                    <h1 className="text-5xl font-bold font-[poppins]">StatusCast</h1>
+                    <p className="py-6 font-[poppins]">
                         “Broadcast Your Presence, Online”
                     </p>
-                    <Link to={"/home"} className="btn btn-primary">Get Started</Link>
+                    <Link to={"/home"} className="btn btn-primary font-[roboto]">Get Started</Link>
                 </div>
             </div>
         </div>
