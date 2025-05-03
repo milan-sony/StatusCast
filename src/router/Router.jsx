fbfd517c7932f67316d2f08274ac45ae4a9afcc0
Milan Sony <milansonyofficial@gmail.com>
2025-05-03 14:27:14 +0530

    feat: 404 path added

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes, Route } from "react-router";
 import IndexPage from '../pages/IndexPage/IndexPage';
 import HomePage from '../pages/HomePage/HomePage';
+import PageNotFound from '../pages/PageNotFound/PageNotFound';
 
 function Router() {
     return (
         <Routes>
             <Route path='/' element={<IndexPage />} />
             <Route path='/home' element={<HomePage />} />
+            <Route path='*' element={<PageNotFound />} />
         </Routes>
     )
 }
