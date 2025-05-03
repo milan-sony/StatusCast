809b2c4a7f56fda83d35e73951d514f267a90f05
Milan Sony <milansonyofficial@gmail.com>
2025-05-03 17:08:33 +0530

    feat: toaster added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Router from './router/Router'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
     <div>
       <Router />
+      <Toaster />
     </div>
   )
 }
