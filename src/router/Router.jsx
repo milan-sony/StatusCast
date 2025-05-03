16efd0f7d430be59e282caaa4d03987d17946411
Milan Sony <milansonyofficial@gmail.com>
2025-05-03 15:18:58 +0530

    feat: login and signup router added

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,12 +3,16 @@ import { Routes, Route } from "react-router";
 import IndexPage from '../pages/IndexPage/IndexPage';
 import HomePage from '../pages/HomePage/HomePage';
 import PageNotFound from '../pages/PageNotFound/PageNotFound';
+import SignupPage from '../pages/SignupPage/SignupPage';
+import LoginPage from '../pages/LoginPage/LoginPage';
 
 function Router() {
     return (
         <Routes>
             <Route path='/' element={<IndexPage />} />
             <Route path='/home' element={<HomePage />} />
+            <Route path='/signup' element={<SignupPage />} />
+            <Route path='/login' element={<LoginPage />} />
             <Route path='*' element={<PageNotFound />} />
         </Routes>
     )
