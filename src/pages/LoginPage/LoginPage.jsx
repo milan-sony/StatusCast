b97ae32a6a20828c7233844c8430ca7568f51fa2
Milan Sony <milansonyofficial@gmail.com>
2025-05-03 15:16:47 +0530

    style: login page created

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+function LoginPage() {
+    return (
+        <>
+            <div className="w-full h-screen overflow-hidden bg-base-200 flex flex-col">
+                <Navbar />
+                <div className="hero h-screen">
+                    <div className="hero-content flex-col lg:flex-row-reverse max-w-4xl">
+                        <div className="text-center lg:text-left">
+                            <h1 className="text-5xl font-bold">Login now!</h1>
+                            <p className="py-6">
+                                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                quasi. In deleniti eaque aut repudiandae et a id nisi.
+                            </p>
+                        </div>
+                        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                            <div className="card-body">
+                                <fieldset className="fieldset">
+                                    <label className="label">Email</label>
+                                    <input type="email" className="input" placeholder="Email" />
+                                    <label className="label">Password</label>
+                                    <input type="password" className="input" placeholder="Password" />
+                                    <div><a className="link link-hover">Forgot password?</a></div>
+                                    <button className="btn btn-neutral mt-4">Login</button>
+                                </fieldset>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LoginPage
