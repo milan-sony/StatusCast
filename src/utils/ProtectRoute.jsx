83e0adc4fd0036e04ab950f7866743176acc887b
Milan Sony <milansonyofficial@gmail.com>
2025-05-05 00:24:03 +0530

    style: code changed

--- src/utils/ProtectRoute.jsx
+++ src/utils/ProtectRoute.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
 import { userAuthStore } from "../store/authStore";
-import { Navigate } from "react-router";
+import { useNavigate } from "react-router";
+
+// const ProtectedRoute = ({ children }) => {
+//     const accessToken = userAuthStore(state => state.accessToken)
+//     console.log("Protected route accessToken, ", accessToken)
+//     return accessToken ? children : <Navigate to="/login" />
+// }
+
+// export default ProtectedRoute
 
 const ProtectedRoute = ({ children }) => {
-    const accessToken = userAuthStore(state => state.accessToken)
-    return accessToken ? children : <Navigate to="/login" />
+
+    const { isUserAuthenticated } = userAuthStore()
+
+    console.log("isUserAuthenticated in protected", isUserAuthenticated)
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!isUserAuthenticated) navigate("/login")
+    }, [])
+
+    return children
 }
 
 export default ProtectedRoute
