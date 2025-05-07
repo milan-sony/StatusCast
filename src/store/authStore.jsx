9ec70bc8c2196411cf0b11c2f488ce42cbaa7ad6
MNP-GH-417087 <417087@manappuram.com>
2025-05-07 12:11:57 +0530

    feat: user redirect to home page after they signup

--- src/store/authStore.jsx
+++ src/store/authStore.jsx
@@ -8,6 +8,8 @@ export const userAuthStore = create((set) => ({
     accessToken: null,
     isUserAuthenticated: false,
     isLoading: true,
+    isSigningUp: false,
+    isLoggingIn: false,
 
     setUser: (user) => set({ user }),
     setAccessToken: (accessToken) => set({ accessToken }),
@@ -15,19 +17,40 @@ export const userAuthStore = create((set) => ({
     setLoading: (isLoading) => set({ isLoading }),
 
     signup: async (data, navigate) => {
+        set({ isSigningUp: true })
         try {
+            console.log("data", data)
             const res = await axiosInstance.post("/auth/signup", data)
             if (res.data?.status === 201) {
                 toast.success(res.data?.message || "Account successfully created!")
-                navigate("/login")
+
+                const loginData = {
+                    email: data.email,
+                    password: data.password
+                }
+
+                const loginRes = await axiosInstance.post("/auth/login", loginData)
+
+                if (loginRes.data?.status === 200) {
+                    set({
+                        user: loginRes.data?.data,
+                        accessToken: loginRes.data?.token,
+                        isUserAuthenticated: true
+                    })
+                    toast.success(loginRes.data?.message || "Login successfull")
+                    navigate("/home")
+                }
             }
         } catch (error) {
-            console.error("Error signing up the user, ", error)
+            console.error("Error signing up the user, ", error.message)
             toast.error(error.response?.data?.message)
+        } finally {
+            set({ isSigningUp: false })
         }
     },
 
     login: async (data, navigate) => {
+        set({ isLoggingIn: true })
         try {
             const res = await axiosInstance.post("/auth/login", data)
             if (res.data?.status === 200) {
@@ -42,6 +65,8 @@ export const userAuthStore = create((set) => ({
         } catch (error) {
             console.error("Error logging up the user, ", error.message)
             toast.error(error.response?.data.message || "Invalid credentials")
+        } finally {
+            set({ isLoggingIn: false })
         }
     },
 
