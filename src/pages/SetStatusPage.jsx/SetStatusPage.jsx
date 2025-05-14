3ff36dc1d45c5884df9ae17857df7b4932f5546d
Milan Sony <milansonyofficial@gmail.com>
2025-05-14 17:36:52 +0530

    style: designing status page

--- src/pages/SetStatusPage.jsx/SetStatusPage.jsx
+++ src/pages/SetStatusPage.jsx/SetStatusPage.jsx
@@ -1,10 +1,30 @@
+import { Smile } from 'lucide-react'
 import React from 'react'
 
 function SetStatusPage() {
     return (
-        <div>
-            <h1>Set Status</h1>
-        </div>
+        <>
+            <div className='w-full h-screen overflow-hidden'>
+                <div className='mt-20 pl-10 pr-10'>
+
+                    <div>
+                        <h1 className='text-2xl font-bold font-[poppins] mb-5'>Set Your Status</h1>
+
+                        <div className="join">
+                            <div>
+                                <span className="input join-item">
+                                    <input type="text" className='text-gray-600' placeholder="Eg. Focusing" required />
+                                </span>
+                            </div>
+                            <button className="btn btn-square">
+                                <Smile/>
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
