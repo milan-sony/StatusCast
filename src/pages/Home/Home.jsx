956aa95fe7fe364203536525bfbe7bc69e9d4a1f
Milan Sony <milansonyofficial@gmail.com>
2025-06-15 11:52:55 +0530

    style: ui modidied

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -26,21 +26,19 @@ function Home() {
                 <div className='pt-[100px] pl-10 pr-10'>
                     <div className='flex flex-col justify-center items-center sm:flex-row sm:justify-between text-center'>
                         <div>
-                            <h1 className='text-xl sm:text-4xl font-bold font-[poppins] capitalize'>Hi {user?.firstName} | {user?.userName} <span className='badge badge-soft badge-success badge-xs'>Active</span></h1>
+                            <h1 className='text-xl sm:text-4xl font-bold font-[poppins]'> <span className='capitalize'>Hi {user?.firstName} </span> | <span>{user?.userName}</span> <span className='badge badge-soft badge-success badge-xs'>Online</span></h1>
                         </div>
                         <div className='mt-3'>
                             <Link to={"/search-profile"} className='btn btn-primary btn-soft btn-sm md:btn-md font-[roboto] mr-2'>Find Profile</Link>
                             <Link to={"/set-status"} className='btn btn-primary btn-sm md:btn-md font-[roboto]  ml-2'>Set Status</Link>
                         </div>
                     </div>
-
                     {/* Current status */}
                     <div className='mt-10'>
                         <h1 className='font-bold text-md mb-2 font-[roboto]'>Your status</h1>
                         <div className='w-full flex justify-center'>
                             {
                                 userStatus ? (
-
                                     <div className='flex flex-col justify-center items-center text-center p-4 bg-base-200 rounded-box sm:w-md sm:flex-row sm:justify-between sm:text-left sm:items-center shadow-md'>
                                         <div className='flex justify-center'>
                                             <img className="size-15 sm:size-10" src={userStatus.emoji} alt="emoji" />
@@ -56,17 +54,11 @@ function Home() {
                                             <button className="btn btn-sm btn-soft btn-error" onClick={statusDelete}><Trash2 size={15} /></button>
                                         </div>
                                     </div>
-
                                 ) : (
-
-                                    <div className='flex flex-col justify-center items-center text-center p-4 bg-base-100 rounded-box sm:w-md sm:flex-row sm:justify-between sm:text-left sm:items-center'>
-                                        <div className='flex justify-center'>
-                                            <p className='font-roboto text-sm'>You haven't set any status!</p>
-                                        </div>
+                                    <div className='w-full flex justify-center'>
+                                        <p className='font-roboto text-sm opacity-60'>You haven't set any status!</p>
                                     </div>
-
                                 )
-
                             }
                         </div>
                     </div>
@@ -74,18 +66,15 @@ function Home() {
                     {/* Others status  */}
                     <div className='mt-10'>
                         <h1 className='font-bold text-md mb-2 font-[roboto]'>Other's status</h1>
-
-                        <div className='w-full max-h-[200px] overflow-y-auto p-2 flex justify-center'>
-                            <div className='grid grid-cols-1 xl:grid-cols-3 gap-4'>
-                                {
-                                    allUserStatus.length === 0 ? (
-                                        <div className='flex flex-col justify-center items-center text-center p-4 bg-base-100 rounded-box sm:w-md sm:flex-row sm:justify-between sm:text-left sm:items-center'>
-                                            <div className='flex justify-center'>
-                                                <p className='font-roboto text-sm'>No one has set any status!</p>
-                                            </div>
-                                        </div>
-                                    ) : (
-                                        allUserStatus.map((status) => (
+                        {
+                            allUserStatus.length === 0 ? (
+                                <div className='w-full flex justify-center'>
+                                    <p className='font-roboto text-sm opacity-60'>No one has set any status yet!</p>
+                                </div>
+                            ) : (
+                                allUserStatus.map((status) => (
+                                    <div className='w-full max-h-[200px] overflow-y-auto p-2 flex justify-center'>
+                                        <div className='grid grid-cols-1 xl:grid-cols-3 gap-4'>
                                             <div className='flex flex-col justify-center items-center text-center p-4 bg-base-200 rounded-box sm:justify-between sm:w-sm sm:flex-row sm:text-left shadow-md' key={status._id}>
                                                 <div className='flex justify-center'>
                                                     <img className="size-15 sm:size-10" src={status.emoji} alt="emoji" />
@@ -99,11 +88,11 @@ function Home() {
                                                     </div>
                                                 </div>
                                             </div>
-                                        ))
-                                    )
-                                }
-                            </div>
-                        </div>
+                                        </div>
+                                    </div>
+                                ))
+                            )
+                        }
                     </div>
                 </div>
             </div>
