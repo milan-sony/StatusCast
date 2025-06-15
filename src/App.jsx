9ce6f4586f94f5ea3015d1b83d7646465782aa31
Milan Sony <milansonyofficial@gmail.com>
2025-06-15 10:02:04 +0530

    style: code formatted

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,43 @@ import Navbar from './components/Navbar/Navbar'
 import PageLoader from './components/Loader/PageLoader'
 
 function App() {
-  const { setAccessToken, setUserAuthenticate, setLoading, isLoading, setUser } = userAuthStore()
+  const { setAccessToken, setUserAuthenticate, setLoading, isLoading, setUser, user } = userAuthStore()
   const [showLoader, setShowLoader] = useState(false) // State to control loader visibility
   const loaderDelay = 1000 // Delay in milliseconds
 
   useEffect(() => {
     const checkRefreshToken = async () => {
+
       setLoading(true) // Start loading
       setShowLoader(true) // Show loader immediately
+
       try {
+
         const res = await axiosInstance.get("/auth/refresh", { withCredentials: true })
+
         setAccessToken(res.data?.accessToken)
         setUserAuthenticate(true)
 
         // Fetch user profile after refresh success
         const profileRes = await axiosInstance.post("/user/profile")
+
         if (profileRes.data?.status === 200) {
           setUser(profileRes.data?.user)
         }
 
-
       } catch (error) {
+
         console.error("Not authenticated", error.message)
+        setUserAuthenticate(false)
+
       } finally {
+
         setLoading(false) // End loading
         // Set a timeout to hide the loader after the delay
         setTimeout(() => {
           setShowLoader(false)
         }, loaderDelay)
+
       }
     }
 
